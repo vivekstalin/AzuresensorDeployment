c201e6b9ca993e030470b4d6cc3b31d536525cf2
vivekstalin <vivek_stalin@mcafee.com>
2018-01-23 17:38:43 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,12 @@ do
       sleep 2
 done
 echo "/var/log/cloud-init-output.log file found"
-while ! grep "Finished vNSP deployment" /var/log/cloud-init-output.log
+while ! grep "Finished vNSP init setup" /var/log/cloud-init-output.log
 do
   sleep 5;
 done
 echo "\"Finished vNSP init setup\" string found"
+cp -r /root/demodata.txt .
+cp -r /root/controller_custom_data.txt .
+cp -r /root/sensor_custom_data.txt .
 python cloudAPIAutomation_Azure.py
