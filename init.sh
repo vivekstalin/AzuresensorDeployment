aaede1be2ad376e700f244c780483fe53601ecca
vivekstalin <vivek_stalin@mcafee.com>
2018-01-23 17:15:02 +0530

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while [ ! -f /var/log/cloud-init-output.log ] ;
+do
+      sleep 2
+done
+echo "/var/log/cloud-init-output.log file found"
+while ! grep "Finished vNSP deployment" /var/log/cloud-init-output.log
+do
+  sleep 5;
+done
+echo "\"Finished vNSP init setup\" string found"
